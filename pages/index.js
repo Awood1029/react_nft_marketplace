c5960d4ebb118df3de116988c48434700bb44a69
Arthur Wood <cwarthur1029@gmail.com>
2022-06-29 23:47:32 -0400

    Finished Create NFT Page and added Hardhat

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
-import { Banner, CreatorCard } from '../components';
+import { Banner, CreatorCard, NFTCard } from '../components';
 import images from '../assets';
 import { makeId } from '../utils/makeId';
 
@@ -10,10 +10,10 @@ const Home = () => {
   const { theme } = { useTheme };
   const parentRef = useRef(null);
   const scrollRef = useRef(null);
-  const scollAmount = window.innerWidth > 1800 ? 270 : 210;
 
   const handleScroll = (direction) => {
     const { current } = scrollRef;
+    const scollAmount = window.innerWidth > 1800 ? 270 : 210;
 
     if (direction === 'left') {
       current.scrollLeft -= scollAmount;
@@ -92,6 +92,30 @@ const Home = () => {
                 </div>
               </>
               )}
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+            <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl text-semibold sm:mb-4 flex-1">Hot Bids</h1>
+            <div>
+              Search Bar
+            </div>
+            <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+                <NFTCard
+                  key={`NFT-${i}`}
+                  nft={{
+                    i,
+                    name: `Nifty NFT ${i}`,
+                    price: (10 - i * 0.5434).toFixed(2),
+                    seller: `0x${makeId(3)}...${makeId(4)}`,
+                    owner: `0x${makeId(3)}...${makeId(4)}`,
+                    description: 'Cool NFT on Sale',
+                  }}
+                />
+              ))}
             </div>
           </div>
         </div>
